7d48416312e67c03821c83e146178f56a977c055
Jyotsna Bhatia <69782687+Jyotsna03@users.noreply.github.com>
2024-06-30 12:54:07 +0530

    Time: 42 ms (11.72%) | Memory: 16.4 MB (90.63%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -5,15 +5,13 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def __init__(self):
+        self.arr = []
+    
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        result = []
-        
-        def inorder(node: Optional[TreeNode]):
-            if node:
-                inorder(node.left)    # Traverse the left subtree
-                result.append(node.val)    # Visit the root
-                inorder(node.right)    # Traverse the right subtree
-        
-        inorder(root)
-        return result 
+        if root:
+            self.inorderTraversal(root.left)    # Traverse the left subtree
+            self.arr.append(root.val)           # Visit the root
+            self.inorderTraversal(root.right)   # Traverse the right subtree
+        return self.arr
         
