a0d69322d4915108e81721491238ecfeb03a4f87
Jyotsna Bhatia <69782687+Jyotsna03@users.noreply.github.com>
2024-07-12 12:23:23 +0530

    Time: 53 ms (36.60%) | Memory: 17.5 MB (19.99%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,12 +1,6 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        # In case of different length of thpse two strings...
-        if len(s) != len(t):
-            return False
-        for idx in set(s):
-            # Compare s.count(l) and t.count(l) for every index i from 0 to 26...
-            # If they are different, return false...
-            if s.count(idx) != t.count(idx):
-                return False
-        return True 
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        # sorted          - time O (n log n)
+        return sorted(s) == sorted(t)
         
