f11ed6af614421b9661949e280b3ae12c0b9d0e0
Jyotsna Bhatia <69782687+Jyotsna03@users.noreply.github.com>
2024-10-19 14:50:27 +0530

    Time: 15 ms (100.00%) | Memory: 16.7 MB (99.94%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,5 +2,19 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
         # sorted          - time O (n log n)
-        return sorted(s) == sorted(t)
+        #return sorted(s) == sorted(t)
+
+        
+        if len(s) != len(t): return False
+        
+        hashMap = {}
+        for char in s: 
+            hashMap[char] = hashMap.get(char, 0) + 1
+        
+        for char in t:
+            if char not in hashMap or hashMap[char] == 0:
+                return False
+            else: hashMap[char] -= 1
+        
+        return True
         
